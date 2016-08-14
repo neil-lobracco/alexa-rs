f2321cba7f1adb3d0d7dfde575a8c9f422129f42
Neil LoBracco <neil.lobracco@gmail.com>
2016-08-13 21:35:51 -0400

    it builds, wow.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 extern crate iron;
+extern crate chrono;
 extern crate bodyparser;
 extern crate serde_json;
-
+include!(concat!(env!("OUT_DIR"), "/request.rs"));
 pub trait RequestHandler {
     fn handle_request(&self, &Request) -> Response;
 }
@@ -9,7 +10,9 @@ pub trait RequestHandler {
 struct IronHandler {
 }
 impl iron::middleware::Handler for IronHandler {
-    fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        Result<Request> = Request::from(req);
+    fn handle(&self, req: &mut iron::Request) -> iron::prelude::IronResult<iron::prelude::Response> {
+        let res: Result<Request,&'static str> = Request::from(req);
+        Ok(iron::Response::with((iron::status::Ok, "hi")))
     }
 }
+pub struct Response {}
