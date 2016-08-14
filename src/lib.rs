eaa3183c06d7d39d9087d72d8c28bce3abdc2659
Neil LoBracco <neil.lobracco@gmail.com>
2016-08-13 22:44:34 -0400

    Fix warnings, implement app id checking.

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,22 @@ pub trait RequestHandler {
     fn handle_request(&self, &Request) -> Response;
 }
 
-struct IronHandler {
+pub struct IronHandler {
+    application_id: String,
 }
 impl iron::middleware::Handler for IronHandler {
     fn handle(&self, req: &mut iron::Request) -> iron::prelude::IronResult<iron::prelude::Response> {
-        let res: Result<Request,&'static str> = Request::from(req);
-        Ok(iron::Response::with((iron::status::Ok, "hi")))
+        let res: Result<Request,&'static str> = Request::from(req,&self.application_id);
+        match res {
+            Ok(_) => Ok(iron::Response::with((iron::status::Ok, "hi"))),
+            Err(s) => Ok(iron::Response::with((iron::status::BadRequest, s))),
+        }
+
+    }
+}
+impl IronHandler {
+    pub fn new(application_id: String) -> IronHandler {
+        IronHandler { application_id: application_id }
     }
 }
 pub struct Response {}
